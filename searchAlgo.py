cb07bd8660b7d86d5df69e12c909a66cee4fbd24
Samuel Tardif <samhockey04@hotmail.com>
2022-12-02 12:29:02 -0500

    UCS added

--- searchAlgo.py
+++ searchAlgo.py
@@ -63,11 +63,132 @@ class SearchNode:
 
 # AYYYYY
 
-def searchUCS(state):
+def searchUCS(name, startState):
+
+    #Setting files for writing as we go
+    algoName = "UCS"
+    solFileName = algoName.append("-sol-")
+    solFileName = solFileName.append(algoName)
+    solFileName = solFileName.append(".txt")
+
+    searchFileName = algoName.append("-search-")
+    searchFileName = searchFileName.append(algoName)
+    searchFileName = searchFileName.append(".txt")
+
+    #Setting up lists
+    openList = [SearchNode(startState,0,"","none",0)]
+    closedList = []
+
+    #Counter to know how many nodes we explore
+    counter = 0
+
+    while openList != []:
+
+        counter += 1
+
+        current = open.pop(0)
+
+        if rh.isSolution(current.state):
+            #We found it
+            solution = returnSolutionPath(current)
+            print(solution)
+            #TO DO SOLUTION FILE
+            #
+            #
+            #
+            #
+            #
+            #
+            #
+            #
+            #
+            #
+
+
+            #Do this so we stop looking
+            return "Solution found: "+solution
+        else:
+            #Children of current
+            children = []
+            activeCars = rh.listOfCars(current.state)
+            for car in activeCars:
+                #Check if we can move right
+                if rh.getCarRangeRight(car, current.state) > 0:
+                    for dist in range(1,rh.getCarRangeRight(car, current.state)):
+                        children.append(SearchNode(rh.moveCarRight(car,current.state,dist),
+                                                   current.cost+dist,
+                                                   str(car)+"\tright\t"+str(dist),
+                                                   current,
+                                                   0))
+                # Check if we can move left
+                if rh.getCarRangeLeft(car, current.state) > 0:
+                    for dist in range(1, rh.getCarRangeLeft(car, current.state)):
+                        children.append(SearchNode(rh.moveCarLeft(car, current.state, dist),
+                                                   current.cost + dist,
+                                                   str(car) + "\tleft\t" + str(dist),
+                                                   current,
+                                                   0))
+                # Check if we can move up
+                if rh.getCarRangeUp(car, current.state) > 0:
+                    for dist in range(1, rh.getCarRangeUp(car, current.state)):
+                        children.append(SearchNode(rh.moveCarUp(car, current.state, dist),
+                                                   current.cost + dist,
+                                                   str(car) + "\tup\t" + str(dist),
+                                                   current,
+                                                   0))
+                # Check if we can move down
+                if rh.getCarRangeDown(car, current.state) > 0:
+                    for dist in range(1, rh.getCarRangeDown(car, current.state)):
+                        children.append(SearchNode(rh.moveCarDown(car, current.state, dist),
+                                                    current.cost + dist,
+                                                    str(car) + "\tdown\t" + str(dist),
+                                                    current,
+                                                    0))
+
+            #Add current to closed list
+            closedList.append(current)
+
+            #Check if children are in closed
+            for child in children:
+                for node in closedList:
+                    if rh.areStatesSame(child.state, node.state):
+                        children.remove(child)
+
+
+            #Check if children are in open
+            for child in children:
+                for node in openList:
+                    if rh.areStatesSame(child.state, node.state):
+                        children.remove(child)
+
+            #Insert in queue according to cost
+            for child in children:
+                if len(openList) == 0:
+                    openList.append(child)
+                else:
+                    for i in range(0, len(openList)):
+                        if child.cost <= openList[i].cost:
+                            openList.insert(i, child)
+
+
+    #NO solution was found
+    #TO DO - Print solution file with no sol
+    #
+    #
+    #
+    #
+    #
+    #
+    #
+
+    print("no solution")
+    return "No solution"
+
+def searchGBFS(name, startState):
     return
 
-def searchGBFS(state):
+def searchA(name, startState):
     return
 
-def searchA(state):
+def returnSolutionPath(solutionNode):
     return
